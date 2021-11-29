2688125f520eebde6e939033a9867399d06511e9
Matthew Lewis <matthew.d.lewis64@gmail.com>
2021-11-29 09:33:43 -0600

    Completed ten-functions, throws 3 errors in jasmine, but passes tests in IDE?

--- solutions.js
+++ solutions.js
@@ -20,3 +20,50 @@
  * and
  * concat
  */
+
+// Problem #1
+let isTrue = boolean => boolean === true;
+console.log('isTrue: ' + isTrue(true));
+
+// Problem #2
+let isFalse = boolean => boolean === false;
+console.log('isFalse: ' + isFalse(true));
+
+// Problem #3
+let not = boolean => !boolean;
+console.log(`not: ${not(false)}`);
+
+// Problem #4
+let addOne = num => num + 1;
+console.log(`addOne: ${addOne(0)}`);
+
+// Problem #5
+let isEven = num => num % 2 === 0;
+console.log(`isEven: ${isEven(4)}`);
+
+// Problem #6
+let isIdentical = (num1, num2) => num1 === num2;
+console.log(`isIdentical: ${isIdentical(5,4)}`);
+
+// Problem #7
+let isEqual = (num1, num2) => num1 == num2;
+console.log(`isEqual: ${isEqual(7, '7')}`);
+
+// Problem #8
+let or = (bool1, bool2) => bool1 || bool2;
+console.log(`or: ${or(false, false)}`);
+
+// Problem #9
+let and = (bool1, bool2) => bool1 && bool2;
+console.log(`and: ${and(true, false)}`);
+
+// Problem #10
+function concat (a, b) {
+    if (typeof a === "string" && typeof b === "string") {
+        return a + b;
+    } else if(typeof a === "number" && typeof b === "number") {
+        return (a.toString() + b.toString());
+    }
+}
+
+console.log(`concat: ${concat('blue','red')}`);
